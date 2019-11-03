6e54cc88074f3cd329c4f3474dfaa71f40aedacd
Giedrius Bielskis <giedrius.bielskis@gmail.com>
2019-11-03 20:36:33 +0200

    add markdown config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    }
+  ],
 }
